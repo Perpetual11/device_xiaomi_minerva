68231a65e69db6fe42e5e3a8415bf5123f87b4f4
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-07-23 08:54:08 +0700

    minerva: Define AAPT configuration
    Change-Id: I261e16476b763277627af1ac803319102ac9f806
    Signed-off-by: Vayruz Rafli <vayruzrafli79@outlook.co.id>

--- device-common.mk
+++ device-common.mk
@@ -13,6 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
