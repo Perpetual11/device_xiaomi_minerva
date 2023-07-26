0831bee19318644c5fc8749318e2fe0bf80f3fec
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-07-26 18:58:49 +0700

    minerva: Import stock IRQ balance configuration file
    Change-Id: Ie8617b443d79b4ebb9dac79378502d0d52ef4a0b
    Signed-off-by: Vayruz Rafli <vayruzrafli79@outlook.co.id>

--- device-common.mk
+++ device-common.mk
@@ -267,6 +267,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# IRQ balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
