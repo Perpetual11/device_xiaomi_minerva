beca73fb3ece985300559e05b409cfa076470959
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-08-05 16:26:33 +0700

    minerva: Correctly inherit common Syberia configuration
    Change-Id: I28ddd09d4570c13dc023ceb65826a49c94a0ddc3
    Signed-off-by: Vayruz Rafli <vayruzrafli79@outlook.co.id>

--- minerva.mk
+++ minerva.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Syberia Project configurations.
-$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
+$(call inherit-product, vendor/syberia/common.mk)
 
 # Enable AOSP recovery flashing.
 TARGET_USES_AOSP_RECOVERY := true
